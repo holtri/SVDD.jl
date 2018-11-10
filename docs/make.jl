723013e5c3727976b50ab31e295ec8497e14d737
Holger <holger.trittenbach@gmail.com>
2018-11-10 15:32:53 +0100

    add SMO documentation

--- docs/make.jl
+++ docs/make.jl
@@ -2,11 +2,17 @@ using Documenter
 using SVDD
 
 makedocs(
-    sitename = "SVDD",
+    sitename = "SVDD Documentation",
+    pages = [
+        "Home" => "index.md",
+        "Getting Started" => "start.md",
+        "Custom Solvers" => [
+            "SMO" => "smo.md"
+            ]
+        ],
     format = :html,
     modules = [SVDD]
 )
-
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
